0197e5e2806090b9174485e7c5ae56312ad607a4
yangss3 <yss_2016@outlook.com>
2021-05-23 15:56:16 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
 #!/usr/bin/env node
+import path from 'path'
+import fs from 'fs'
+import { exec as execCb } from 'child_process'
+import { promisify } from 'util'
+import ora from 'ora'
+
+const spinner = ora()
+const exec = promisify(execCb)
+const cwd = process.cwd()
+console.log(cwd)
+console.log(__dirname)
+const gitPath = path.resolve(cwd, '.git')
+
+const scripts = [
+  `cd ${cwd}`,
+  'npx mrm@2 lint-staged',
+  'npx husky add .husky/commit-msg "node ./node_modules/@yangss/init-git-repo/scripts/validate-commit-msg.js"'
+]
+async function init () {
+  try {
+    fs.accessSync(gitPath, fs.constants.F_OK)
+  } catch (error) {
+    scripts.splice(1, 0, 'git init')
+  }
+  spinner.start('Initialize git Repo...')
+  await exec(scripts.join('&&'))
+  spinner.succeed('Initialize git Repo completed')
+}
+
+init().catch(e => {
+  console.error(e)
+})
